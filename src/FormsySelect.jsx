01ec4b2ccd6325731f6c36159900b48cecb1c0bf
D.Toloknov <raywen@mail.ru>
2017-07-21 13:02:20 +0300

    ErrorTooltip for FormsySelect

--- src/FormsySelect.jsx
+++ src/FormsySelect.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Formsy from 'formsy-react';
 import SelectField from 'material-ui/SelectField';
 import { setMuiComponentAndMaybeFocus } from './utils';
+import ErrorTooltip from './ErrorTooltip';
 
 const FormsySelect = React.createClass({
 
@@ -12,6 +13,7 @@ const FormsySelect = React.createClass({
     requiredError: React.PropTypes.string,
     validationError: React.PropTypes.string,
     validationErrors: React.PropTypes.object,
+    errorStyle: React.PropTypes.object,
     validations: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.object]),
     value: React.PropTypes.any,
   },
@@ -34,6 +36,13 @@ const FormsySelect = React.createClass({
     if (this.props.onChange) this.props.onChange(event, value, index);
   },
 
+  hasError() {
+    if (this.isRequired() && !this.isPristine() && !this.isValid() && this.isFormSubmitted()) {
+      return true;
+    }
+    return false;
+  },
+
   setMuiComponentAndMaybeFocus: setMuiComponentAndMaybeFocus,
 
   render() {
@@ -48,17 +57,27 @@ const FormsySelect = React.createClass({
     const { requiredError } = this.props;
     const { isRequired, isPristine, isValid, isFormSubmitted } = this;
     const isRequiredError = isRequired() && !isPristine() && !isValid() && isFormSubmitted() && requiredError;
-    const errorText = this.getErrorMessage() || isRequiredError;
+    const errorText = this.getErrorMessage() || this.hasError() && requiredError;
+    const errorTooltipStyles = Object.assign({}, this.props.errorStyle, errorTooltipStyle);
     return (
       <SelectField
         {...rest}
-        errorText={errorText}
         onChange={this.handleChange}
         ref={this.setMuiComponentAndMaybeFocus}
         value={value}
       >
         {this.props.children}
       </SelectField>
+      {
+        errorText ? (
+          <ErrorTooltip
+            style={errorTooltipStyles}
+            ugStyle={{ borderRightColor: errorTooltipStyles.background || {}}}
+          >
+            {errorText}
+          </ErrorTooltip>
+        ) : ''
+      }
     );
   },
 });
