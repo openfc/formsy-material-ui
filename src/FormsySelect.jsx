5d71cea94d244784b062f2f185dee5215163e3f8
D.Toloknov <raywen@mail.ru>
2017-07-24 14:06:11 +0300

    FormsySelect

--- src/FormsySelect.jsx
+++ src/FormsySelect.jsx
@@ -8,12 +8,21 @@ const FormsySelect = React.createClass({
 
   propTypes: {
     children: React.PropTypes.node,
+    className: React.PropTypes.string,
+    errorStyle: React.PropTypes.object,
+    errorTooltipStyle: React.PropTypes.object,
+    inputClass: React.PropTypes.string,
+    inputErrorClass: React.PropTypes.string,
+    inputErrorStyle: React.PropTypes.object,
     name: React.PropTypes.string.isRequired,
     onChange: React.PropTypes.func,
+    required: React.PropTypes.bool,
     requiredError: React.PropTypes.string,
+    style: React.PropTypes.object,
+    underlineFocusStyle: React.PropTypes.object,
+    underlineStyle: React.PropTypes.object,
     validationError: React.PropTypes.string,
     validationErrors: React.PropTypes.object,
-    errorStyle: React.PropTypes.object,
     validations: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.object]),
     value: React.PropTypes.any,
   },
@@ -43,41 +52,61 @@ const FormsySelect = React.createClass({
     return false;
   },
 
+  validationColor(props = this.props) {
+    return props.validationColor || '#DA0000';
+  },
+
   setMuiComponentAndMaybeFocus: setMuiComponentAndMaybeFocus,
 
   render() {
     let { value } = this.props;
 
-    const { validations, // eslint-disable-line no-unused-vars
-        validationError, // eslint-disable-line no-unused-vars
-        validationErrors, // eslint-disable-line no-unused-vars
-      ...rest } = this.props;
+    const {
+      validations, // eslint-disable-line no-unused-vars
+      validationError, // eslint-disable-line no-unused-vars
+      validationErrors, // eslint-disable-line no-unused-vars
+      required, // eslint-disable-line no-unused-vars
+      underlineStyle,
+      underlineFocusStyle,
+      errorTooltipStyle,
+      requiredError,
+      className,
+      style,
+      ...rest,
+      } = this.props;
 
     value = this.state.hasChanged ? this.getValue() : value;
-    const { requiredError } = this.props;
-    const { isRequired, isPristine, isValid, isFormSubmitted } = this;
-    const isRequiredError = isRequired() && !isPristine() && !isValid() && isFormSubmitted() && requiredError;
     const errorText = this.getErrorMessage() || this.hasError() && requiredError;
     const errorTooltipStyles = Object.assign({}, this.props.errorStyle, errorTooltipStyle);
+
     return (
-      <SelectField
-        {...rest}
-        onChange={this.handleChange}
-        ref={this.setMuiComponentAndMaybeFocus}
-        value={value}
-      >
-        {this.props.children}
-      </SelectField>
-      {
-        errorText ? (
-          <ErrorTooltip
-            style={errorTooltipStyles}
-            ugStyle={{ borderRightColor: errorTooltipStyles.background || {}}}
-          >
-            {errorText}
-          </ErrorTooltip>
-        ) : ''
-      }
+      <div style={style} className={className}>
+        <SelectField
+          {...rest}
+          onChange={this.handleChange}
+          ref={this.setMuiComponentAndMaybeFocus}
+          value={value}
+          style={style}
+          underlineStyle={this.hasError() ?
+            { borderColor: this.validationColor() } :
+            underlineStyle || { borderBottomColor: '#E0E0E0' }}
+          underlineFocusStyle={this.hasError() ?
+            { borderColor: this.validationColor() } :
+            underlineFocusStyle || { borderBottomColor: '#00AFD2' }}
+        >
+          {this.props.children}
+        </SelectField>
+        {
+          errorText ? (
+            <ErrorTooltip
+              style={errorTooltipStyles}
+              ugStyle={{ borderRightColor: errorTooltipStyles.background || {} }}
+            >
+              {errorText}
+            </ErrorTooltip>
+          ) : ''
+        }
+      </div>
     );
   },
 });
